e7466cb165a3d9b57e0eca780cc4f01f56c067ac
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-04-02 11:58:52 +0700

    Create first stateless widget

---
+++ infopages/lib/location_detail.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Hello, World!"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _section("One!", Colors.red),
+            _section("Two!", Colors.green),
+            _section("Three! aha", Colors.blue)
+          ],
+        ));
+  }
+
+  Widget _section(String title, Color color) //private method
+  {
+    return Container(
+      decoration: BoxDecoration(color: color),
+      child: Text(title),
+    );
+  }
+}
