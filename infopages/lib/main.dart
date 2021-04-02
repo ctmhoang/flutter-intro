7fa73e7d5f7889f8ae35eff69a8876a7723b2537
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-04-02 10:58:46 +0700

    Hello flutter :v

---
+++ infopages/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main(){
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, World!"),
+      ),
+    ),
+  ));
+}
