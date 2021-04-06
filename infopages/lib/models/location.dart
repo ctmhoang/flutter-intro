78b770200704ab41317e658fc9d603feb0bb3f4e
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-04-06 17:24:13 +0700

    Gen code for serialize models

--- infopages/lib/models/location.dart
+++ infopages/lib/models/location.dart
@@ -1,5 +1,10 @@
+import 'package:json_annotation/json_annotation.dart';
+
 import 'location_fact.dart';
 
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   final String name;
   final String url;
@@ -9,4 +14,6 @@ class Location {
       : _facts = facts; // {} optional params
 
   List<LocationFact> get loc_facts => _facts;
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
 }
